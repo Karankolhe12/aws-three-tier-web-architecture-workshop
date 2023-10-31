d863590d61f98f2bcb9646cf212064cb2591d2dc
karan kolhe <karankolhe099@gmail.com>
2023-10-31 16:38:20 +0530

    project

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cr8xqcnjnnpb.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'karan1234',
+    DB_DATABASE : 'webappdb'
+});
